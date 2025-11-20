7b7b72772db98eee9ada81fcb4dbd1acad6b9264
Pujan-Pandey <your-email@example.com>
2025-11-20 13:09:51 +0545

    Even or Odd

--- Day_1/index.js
+++ Day_1/index.js
@@ -13,7 +13,23 @@
 // function in Js
 // Function is a block of code that can perform a specific task
 
-function add(a, b) {
-    return a + b;
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(5, 7));
+
+
+// const doubleNumber = (num) => {
+//     return num * 2;
+// }
+// console.log(doubleNumber(5))
+
+// Even or Odd
+const evenOdd = (num) => {
+    if (num % 2 == 0) {
+        return console.log(`${num} is Even`)
+    } else {
+        return console.log(`${num} is Odd`);
+    }
 }
-console.log(add(5, 7));
+evenOdd(20000);
