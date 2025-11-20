0017504c203362b52de2cad9b970cb2cfc85082b
Pujan-Pandey <your-email@example.com>
2025-11-20 12:56:56 +0545

    Function in Js

--- Day_1/index.js
+++ Day_1/index.js
@@ -1,10 +1,19 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 
-// Variable in Js
-let program = "Hello";
-console.log(program)
+// // Variable in Js
+// let program = "Hello";
+// console.log(program)
 
-// Constant in Js
-const pi = 3.14;
-console.log(pi);
+// // Constant in Js
+// const pi = 3.14;
+// console.log(pi);
+
+
+// function in Js
+// Function is a block of code that can perform a specific task
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 7));
