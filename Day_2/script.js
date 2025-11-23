b1b2986328c26241eee1f58a1122d717a0a1e5b4
Pujan-Pandey <your-email@example.com>
2025-11-23 12:46:25 +0545

    Function

--- Day_2/script.js
+++ Day_2/script.js
@@ -1,10 +1,35 @@
 // Scope in js
 
-let x = 5;
-function test() {
-    const y = 10;
-    console.log(y)
-    console.log(x)
+// let x = 5;
+// function test() {
+//     const y = 10;
+//     console.log(y)
+//     console.log(x)
+// }
+// test();
+// // console.log(y)  Error as it is a local variable/functional scope
+// console.log(x)// Is a global scope variable.
+
+
+
+
+// let score = 50;
+// function updaterScore() {
+//     let score = 100;
+//     console.log(score)
+// }
+// console.log(score);
+// updaterScore();
+// updaterScore();
+// console.log(score);
+
+
+let points = 0;
+function playGame() {
+    let points = 50;
+    points = points + 10;
+    console.log(points);
 }
-// console.log(y)  Error as it is a local variable/functional scope
-console.log(x)// Is a global scope variable.
+console.log(points)
+playGame()
+playGame()
