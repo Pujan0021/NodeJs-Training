4244d5ead3e76fc61e28c316e7b9750ad5f1dcf8
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-15 13:16:33 +0545

    MiddleWare

--- Day_7/index.js
+++ Day_7/index.js
@@ -1,9 +1,13 @@
 let express = require("express");
-let port = "8000";
+let port = "5000";
 let app = express();
+app.use((req, res, next) => {
+    console.log("-----MiddleWare---------");
+    next();
+})
 app.get("/", (req, res) => {
     res.send("Hello From server...")
-})
+});
 app.listen(port, () => {
     console.log("....Server Started....")
-})
+});
