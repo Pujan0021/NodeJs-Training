c93f7a89e8ae38b633d7943d1bdbcf6b5c13439d
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-15 13:48:38 +0545

    Custom MiddleWare

--- Day_3/script.js
+++ Day_3/script.js
@@ -1,89 +1,89 @@
 //async JS
-// setTimeout(() => {
-//     console.log("Hello")
-// }, 300)
-// console.log("Hi");
+setTimeout(() => {
+    console.log("Hello")
+}, 300)
+console.log("Hi");
 
 
 
-// let num = 9;
-// setTimeout(() => {
-//     num = 20
-// }, 10)
-// setTimeout(() => {
-//     console.log("A:", num);
-// }, 0)
+let num = 9;
+setTimeout(() => {
+    num = 20
+}, 10)
+setTimeout(() => {
+    console.log("A:", num);
+}, 0)
 
-// console.log("B:", num)
-
-
-
-
-// console.log("Step 1");
-// setTimeout(() => {
-//     console.log("Step 2")
-// }, 0)
-// setTimeout(() => {
-//     console.log("Step 3");
-// }, 500)
-
-// setTimeout(() => {
-
-//     console.log("Step 4");
-// }, 0)
-// console.log("A");
-// setTimeout(() => {
-//     console.log("B")
-// }, 0)
-// setTimeout(() => {
-//     console.log("C");
-// }, 0)
-
-// setTimeout(() => {
-
-//     console.log("D");
-// }, 50);
-
-//callBack in js
-// const greet = (name, callback) => {
-//     console.log("Hello " + name);
-//     callback();
-// }
-// const askName = () => {
-//     console.log("What is Your")
-// }
-
-// greet("Pujan", askName);
+console.log("B:", num)
 
 
 
-//Promise
-// const data = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         console.log("Welcome")
-//         resolve(200);
-//     }, 20)
-// });
-// async function greetUser() {
-//     console.log("Hello");
-//     await data;
-// }
-// greetUser()
+
+console.log("Step 1");
+setTimeout(() => {
+    console.log("Step 2")
+}, 0)
+setTimeout(() => {
+    console.log("Step 3");
+}, 500)
+
+setTimeout(() => {
+
+    console.log("Step 4");
+}, 0)
+console.log("A");
+setTimeout(() => {
+    console.log("B")
+}, 0)
+setTimeout(() => {
+    console.log("C");
+}, 0)
+
+setTimeout(() => {
+
+    console.log("D");
+}, 50);
+
+callBack in js
+const greet = (name, callback) => {
+    console.log("Hello " + name);
+    callback();
+}
+const askName = () => {
+    console.log("What is Your")
+}
+
+greet("Pujan", askName);
 
 
-// function divide(a, b) {
-//     try {
-//         if (b === 0) throw "Cannot divide by zero";
-//         let result = a / b;
-//         console.log(result);
-//     } catch (err) {
-//         console.log("Error Occured", err);
-//     } finally {
-//         console.log("Done")
-//     }
-// }
-// divide(20, 10);
-// divide(20, 0)
+
+Promise
+const data = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Welcome")
+        resolve(200);
+    }, 20)
+});
+async function greetUser() {
+    console.log("Hello");
+    await data;
+}
+greetUser()
+
+
+function divide(a, b) {
+    try {
+        if (b === 0) throw "Cannot divide by zero";
+        let result = a / b;
+        console.log(result);
+    } catch (err) {
+        console.log("Error Occured", err);
+    } finally {
+        console.log("Done")
+    }
+}
+divide(20, 10);
+divide(20, 0)
 
 
 
