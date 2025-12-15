37aa44b5ea19122d52c64c46b7be9659d008c44b
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-15 13:28:30 +0545

    Custom MiddleWare

--- Day_7/index.js
+++ Day_7/index.js
@@ -5,6 +5,12 @@ app.use((req, res, next) => {
     console.log("-----MiddleWare---------");
     next();
 })
+// Custom MiddleWare
+const middleWare = (req, res, next) => {
+    console.log("This is a custom middleware");
+    next();
+}
+app.use(middleWare);
 app.get("/", (req, res) => {
     res.send("Hello From server...")
 });
