b1574c91a9375c10573b60f6f2d8e60308170d38
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-22 13:27:05 +0545

    File Append and Deletion

--- Day_10/script.js
+++ Day_10/script.js
@@ -2,9 +2,26 @@ const fs = require("fs");
 const path = require("path");
 const fileName = "myFile.txt";
 let filepath = path.join(__dirname, fileName);
-fs.writeFile(filepath, "Hello BCA 2081.", "utf-8", () => {
+// fs.writeFile(filepath, "Hello BCA 2081.", "utf-8", () => {
+//     console.log("File Writing Finished");
+// })
+// fs.appendFile(filepath, "\nFrom Nepathya, College", "utf-8", () => {
+//     console.log("File Appended Finished");
+// })
+const writeFile = async () => {
+    await fs.promises.writeFile(filepath, "Hello BCA 2081.", "utf-8")
     console.log("File Writing Finished");
-})
-fs.appendFile(filepath, "\nFrom Nepathya, College", "utf-8", () => {
+
+}
+writeFile()
+const appendFile = async () => {
+    await fs.promises.appendFile(filepath, "\nFrom Nepathya, College", "utf-8");
     console.log("File Appended Finished");
-})
+}
+appendFile();
+// const deleteFile = async () => {
+//     await fs.promises.unlink(filepath);
+//     console.log("File deletion Finished");
+// }
+// deleteFile();
+
